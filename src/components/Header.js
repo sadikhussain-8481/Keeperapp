408975026e1c4f6423f88bea02bb1c42e4419300
“sadikhussain-8481” <sadikhussainpsb2@gmail.com>
2022-08-01 04:12:49 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+function Header() {
+  return (
+    <header>
+      <h1>
+        <WbSunnyIcon />Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
